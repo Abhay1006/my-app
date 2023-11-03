07a4c4fa5263818c82699acd50ef9cac9e0da292
Abhay1006 <103443256+Abhay1006@users.noreply.github.com>
2023-11-03 10:56:17 +0530

    added new projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -6,5 +6,15 @@ export const projectList = [
         name: 'Todo List App',
         image: proj2,
         skills: 'React.js,HTML,CSS',
+    },
+    {
+        name: 'Learn Velocity',
+        image: proj2,
+        skills: 'React.js,HTML,CSS,javascript,nodejs',
+    },
+    {
+        name: 'Calculator app',
+        image: proj2,
+        skills: 'React.js,HTML,CSS',
     }
 ]
